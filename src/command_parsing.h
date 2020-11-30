d321756849c5a24226af1d6d5687a1c7cd2f9189
4nc3str4l <cmuri_10@hotmail.com>
2020-11-30 18:49:55 +0100

    Moved all the command parsing logic outside from the main function

---
+++ src/command_parsing.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <tgbot/tgbot.h>
+#include <map>
+
+#include "commands/command.h"
+
+
+const std::vector<std::string> getCommandArguments(const std::string &command);
+void exec(TgBot::Bot& bot, Command &c, const std::int64_t& chatId, const std::string& message);
+void parseMessage(TgBot::Bot& bot, std::string message, const std::int64_t& chatId);
