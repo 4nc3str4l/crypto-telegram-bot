024a4be624b04c22ddb2b5db20f89f305d6af3f9
4nc3str4l <cmuri_10@hotmail.com>
2020-11-30 19:37:59 +0100

    Add instructions for portfolio investment set

--- src/commands/portfolio_inv_set.cpp
+++ src/commands/portfolio_inv_set.cpp
@@ -9,7 +9,7 @@ PortfolioInversionSet::PortfolioInversionSet(TgBot::Bot &bot, const std::int64_t
 
 void PortfolioInversionSet::sendInstructions()
 {
-    send(fmt::format("Invalid Command usage {} <>. Example: {} ada", m_command, m_command));
+    send(fmt::format("Invalid Command usage {} <quantity> <ticker>. Example: {} 100 ada", m_command, m_command));
 }
 
 const std::string PortfolioInversionSet::getDescription()
